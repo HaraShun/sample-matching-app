3b4f045ae347846e4e4245209ecb84999ef7b97c
HaraShun <forest.hara0702@outlook.com>
2025-02-05 11:50:55 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
-import outputs from "../amplify/amplify_outputs.json";
+import outputs from "../amplify_outputs.json";
 import './index.css'
 import App from './App.tsx'
 
